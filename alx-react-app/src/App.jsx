aa8b806bcbaf7044203dabcc729e8554d2e6addc
kwakyetech <kwakyeoforiprince@gmail.com>
2025-08-26 16:09:23 +0000

    feat: add Header, MainContent, and Footer components to App

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,17 @@
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import Header from './components/Header'
+import MainContent from './components/MainContent'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
     <WelcomeMessage />
+    <Header />
+    <MainContent />
+    <Footer />
     </>
   )
 }
