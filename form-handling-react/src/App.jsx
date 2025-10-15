7c7ea8273d69138a91116c50605f4992d592352a
kwakyetech <kwakyeoforiprince@gmail.com>
2025-10-15 13:42:54 +0000

    rename the formikForm

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -2,7 +2,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import RegistrationForm from './components/RegistrationForm'
-import FormikForm from './components/formikform';
+import FormikForm from './components/formikForm';
 
 function App() {
   return (
